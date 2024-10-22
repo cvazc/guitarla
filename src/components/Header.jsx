8963421c447690ee0d38abdec12167556b53999c
Sebastian Contreras Arroyo <contreras.sebastian.dev@gmail.com>
2024-10-21 22:35:46 -0600

    Add html, styles and a component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+
+export default function Header() {
+
+    const name = "Sebastian";
+
+    return (
+        <Fragment>
+            <h1>Hola {name}</h1>
+        </Fragment>
+    )
+}
