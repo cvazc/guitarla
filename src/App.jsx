f78bae0bc1f5e7671efbc9b6d9573833db242e7c
Sebastian Contreras Arroyo <contreras.sebastian.dev@gmail.com>
2024-10-24 22:40:33 -0600

    Add new state to add products in cart

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { db } from './data/db'
 function App() {
 
   const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
 
   return (
     <>
@@ -14,8 +15,12 @@ function App() {
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
-          {data.map((index, item) => (
-            <Guitar />
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+            />
           )
           )}
         </div>
